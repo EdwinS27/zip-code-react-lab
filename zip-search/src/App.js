3929d5fca0a22f9731a13a5919aaa447390bcc99
Edwin Samaniego <53633663+EdwinS27@users.noreply.github.com>
2021-10-03 11:53:16 -0400

    Finished Part 1 of zip code Instructor: Edgardo, TA: Silu

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -3,25 +3,80 @@ import './App.css';
 
 
 function City(props) {
-  return (<div>This is the City component</div>);
+  return (
+    <div>
+      <ul>
+        <li> City: {props.cityName} </li>
+        <li> State: {props.State} </li>
+        <li> Location: ({props.Lat}, {props.Long}) </li> 
+        <li> Population: {props.EstimatedPopulation} </li> 
+        <li> Total Wages: {props.TotalWages} </li>
+      </ul>
+    </div>
+  );
 }
 
 function ZipSearchField(props) {
-  return (<div>This is the ZipSearchField component</div>);
+  return (<div>
+    <input onChange={(e) => props.handleChange(e)} />
+    </div>);
 }
 
 
 class App extends Component {
+  state = {
+    zipCode: '',
+    cities: [],
+  }
+
+  zipChange = (event) =>  {
+    console.log(event.target.value);
+
+    this.setState({zipCode: event.target.value});
+
+    // If the target is 5, then we searh for a valid zip
+    if(event.target.value.length === 5) {
+
+      fetch('https://ctp-zip-api.herokuapp.com/zip/' + event.target.value)
+        .then((res) => res.json())
+          .then((results)  => {
+            this.setState({cities: results});
+            console.log({results});
+          })
+          .catch(err =>{
+            console.log("No Results");
+            this.setState({
+              cities: []
+              });
+          })
+          // save the data to my state,
+          // and in the render function display the <City /> components
+          
+    } // end if
+  }
+
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
+        <ZipSearchField handleChange= {(e) => this.zipChange(e)}/>
         <div>
-          <City />
-          <City />
+          Zip code is: {this.state.zipCode}
+        </div>
+        <div>
+          {
+            this.state.cities.map((city) => <City
+            data={city} cityName={city.City}
+            State={city.State}
+            EstimatedPopulation={city.EstimatedPopulation}
+            Long={city.Long}
+            Lat={city.Lat}
+            TotalWages={city.TotalWages}
+            />)
+          }
         </div>
       </div>
     );
